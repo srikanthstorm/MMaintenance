6bcc07d0b028c474353f223706bc236089b9aa7a
Sri Harsha Chilakapati <hello@goharsha.com>
2016-12-16 21:30:57 +0530

    Make sign-up page working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,28 +2,41 @@ angular.module('starter.controllers', [])
 
 //controller for Login
 .controller('LoginCtrl', function($scope, $location, $ionicPopup) {
-  $scope.Login = function() {
-    var uname = document.getElementById('name').value;
-    var pword = document.getElementById('pswd').value;
-    if (uname == "Admin" && pword == "Admin") {
-      $location.path('/app/gohome');
-    } else {
-      $ionicPopup.confirm({
-        title: "Miracle ME alerts you",
-        content: "Invalid username and password."
-      })
+    $scope.Login = function() {
+        var uname = document.getElementById('name').value;
+        var pword = document.getElementById('pswd').value;
+        if (uname == "Admin" && pword == "Admin") {
+            $location.path('/app/gohome');
+        } else {
+            $ionicPopup.confirm({
+                title: "Miracle ME alerts you",
+                content: "Invalid username and password."
+            })
+        }
     }
-  }
-  
+
     $scope.signup = function() {
-$location.path('/signup');
-      }
+        $location.path('/signup');
+    }
 
 })
 //controller for signup
-.controller('SignUpCtrl', function($scope, $location, $ionicPopup) {
-    
+.controller('SignUpCtrl', function($scope, $http, $ionicPopup) {
+    $scope.formData = {};
 
+    $scope.gohome = () => {
+        var details = {
+            uname: $scope.formData.uname,
+            fname: $scope.formData.fname,
+            lname: $scope.formData.lname,
+            mail: $scope.formData.mail,
+            mno: $scope.formData.mno,
+            pwd: $scope.formData.pwd
+        };
+
+        console.log(details);
+        $http.post('http://localhost:4000/signup/', details);
+    };
 })
 
 //controller for  Form
@@ -44,7 +57,7 @@ $location.path('/signup');
 
 //controller for Logout
 .controller('LogoutCtrl', function($scope, $location) {
-  $scope.logout = function() {
-    $location.path('/login');
-  }
+    $scope.logout = function() {
+        $location.path('/login');
+    }
 });
